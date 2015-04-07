3fb099e0976c5b08002c280d4c51b5bf722021ae
theJollySin <thejollysin@gmail.com>
2015-04-06 20:49:57 -0700

    testing git

--- app/src/main/java/net/antineutrino/android/criminalintent/CriminalActivity.java
+++ app/src/main/java/net/antineutrino/android/criminalintent/CriminalActivity.java
@@ -14,7 +14,6 @@ public class CriminalActivity extends ActionBarActivity {
         setContentView(R.layout.activity_criminal);
     }
 
-    // testing
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
