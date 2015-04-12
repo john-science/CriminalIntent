0ae3f106ec6ac3807dd189a8ba2b3185328e1358
theJollySin <thejollysin@gmail.com>
2015-04-12 09:10:40 -0700

    progress on filling list

--- app/src/main/java/net/antineutrino/android/criminalintent/Crime.java
+++ app/src/main/java/net/antineutrino/android/criminalintent/Crime.java
@@ -43,4 +43,9 @@ public class Crime {
     public void setSolved(boolean solved) {
         mSolved = solved;
     }
+
+    @Override
+    public String toString() {
+        return mTitle;
+    }
 }
--- app/src/main/java/net/antineutrino/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/net/antineutrino/android/criminalintent/CrimeListFragment.java
@@ -1,18 +1,65 @@
 package net.antineutrino.android.criminalintent;
 
-import android.support.v4.app.ListFragment;
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
+    private static final String TAG = "CrimeListFragment";
 
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                                                              android.R.layout.simple_list_item_1,
+                                                              mCrimes);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter().getItem(position));
+        Log.d(TAG, c.getTitle() + " was clicked");
+    }
+
+    private class CrimeAdapater extends ArrayAdapter<Crime> {
+
+        public CrimeAdapater(ArrayList<Crime> crimes) {
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // if we weren't given a view, inflate one
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+            }
+
+            // Configure the view for this crime
+            Crime c = getItem(position);
+
+            TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+
+            TextView dateTextView = (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
+        }
     }
 }
